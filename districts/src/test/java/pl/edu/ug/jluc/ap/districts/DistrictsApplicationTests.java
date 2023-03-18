1cbf636d24f0e7fffbae289d124e11ec9d960d71
Justyna Luc <justiluc@interia.pl>
2023-03-18 15:49:04 +0100

    draft of a project

---
+++ districts/src/test/java/pl/edu/ug/jluc/ap/districts/DistrictsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.ug.jluc.ap.districts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistrictsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
